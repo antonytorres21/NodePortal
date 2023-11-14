6f1ec4a5446fe9a5d5de5c47ea3e3305e3bf9ac3
Roig Antony Torres López <72164449+antonytorres21@users.noreply.github.com>
2023-11-13 20:53:12 -0600

    V1

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import indexRoutes from "./routes/index.js";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = process.env.PORT || 3000;
 
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(indexRoutes);
 app.use(express.static(join(__dirname, "public")));
-app.listen(3000);
-console.log("Server is lintening on port", 3000);
+app.listen(port);

52237e6f72c36c124638d1e22272122fc8d2400c
Roig Antony Torres López <72164449+antonytorres21@users.noreply.github.com>
2023-11-14 16:05:12 -0600

    V2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-import indexRoutes from "./src/routes/index.js";
+import indexRoutes from "./routes/index.js";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
